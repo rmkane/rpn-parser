8e82be081b5904f21188c7374ac1109903bd7832
Ryan Kane <rmkane89@gmail.com>
2015-11-11 20:41:46 -0500

    Fixing documentation in example

--- src/main/java/rpn/calculator/core/example/LogExample.java
+++ src/main/java/rpn/calculator/core/example/LogExample.java
@@ -3,10 +3,15 @@ package rpn.calculator.core.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//General configuration:
+// - https://logging.apache.org/log4j/2.x/manual/configuration.html
+// Quick reference:
+// - http://alesnosek.com/blog/2015/05/11/java-logging-quick-reference/
+// JSON configuration:
+// - http://stackoverflow.com/questions/21435990/log4j-2-json-configuration
+// Levels, appenders, and filters:
+// - http://www.journaldev.com/7128/apache-log4j-2-tutorial-configuration-levels-appenders-lookup-layouts-and-filters-example
 public class LogExample {
-	// http://alesnosek.com/blog/2015/05/11/java-logging-quick-reference/
-	// https://logging.apache.org/log4j/2.x/manual/configuration.html
-	// http://stackoverflow.com/questions/21435990/log4j-2-json-configuration
 	private static final Logger LOG = LogManager.getLogger(LogExample.class);
 
 	public static void main(String[] args) {
